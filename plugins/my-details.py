90e8cb17010f32c064b94bdb33d614767ff5334f
KEERTHY SAJI <66038840+ivanstanley143@users.noreply.github.com>
2026-01-04 01:39:43 +0530

    Update my-details.py

--- plugins/my-details.py
+++ plugins/my-details.py
@@ -1,14 +1,9 @@
-# ©️ LISA-KOREA | @LISA_FAN_LK | NT_BOT_CHANNEL | LISA-KOREA/UnZip-Bot
 
-# [⚠️ Do not change this repo link ⚠️] :- https://github.com/LISA-KOREA/UnZip-Bot
+# My Telegram Channel :- @ZKP143
 
-# My Telegram Channel :- @NT_BOT_CHANNEL
+# Support Group :- @ZKP143
 
-# Support Group :- @NT_BOTS_SUPPORT
-
-# My Youtube Channel :- https://youtube.com/@ntbot?si=TJaNuy4e1HqWirqQ
-
-# Developers Usernames :- @JOBIN_JUSTIN , @LISA_FAN_LK
+# Developers Usernames :- @ZKP143
 
 # (C) Don’t Change And Edit This File ✍️ | Warning ⚠️
 
