7dcac2e9aaede50d51cd1a8483298eeec37af824
KEERTHY SAJI <66038840+ivanstanley143@users.noreply.github.com>
2026-01-04 11:35:19 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -19,7 +19,7 @@ class Config(object):
     MAX_FILE_SIZE = 2194304000
     TG_MAX_FILE_SIZE = 2194304000
     FREE_USER_MAX_FILE_SIZE = 2194304000
-    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE",128))
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
     
